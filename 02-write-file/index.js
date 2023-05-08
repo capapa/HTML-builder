e927b0b47457a8ef940eb881a33f276d411d7cf3
capapa <safiev@gmail.com>
2023-05-08 13:47:35 +0500

    feat: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin } = require("process");
+
+const pathFile = path.join(__dirname, "text.txt");
+
+console.log(
+  'Please, write text and click enter.\nFor exit write "exit" or click "ctrl + c"'
+);
+
+const writeStream = fs.createWriteStream(pathFile);
+
+stdin.on("data", (data) => {
+  const str = String(data).trim();
+  if (str === "exit") {
+    process.exit();
+  }
+
+  writeStream.write(str);
+});
+
+process.on("SIGINT", () => {
+  process.exit();
+});
+
+process.on("exit", () => {
+  console.log("End write text, good bye!");
+  writeStream.end();
+});
